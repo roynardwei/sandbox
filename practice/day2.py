81931aaccaeac068c901aac2835934e76f407c19
Roy wei <yuishang@gmail.com>
2024-03-01 15:10:33 +0800

    test1
    python_day_2

---
+++ practice/day2.py
@@ -0,0 +1,25 @@
+# 資料型態
+
+# 字串 string
+name = "你好"
+print(name)
+print("100"+"30")
+#字串特別用法 加中括號
+name = "你好小白"
+print(name[3])
+test
+# 整數 int
+num = 100
+print(num)
+print(100+30)
+
+# 浮點數 float
+num1 = 160.57777
+print(160.57777)
+print(160.5+2.3)
+print(num1)
+# 布林值 boolean
+is_male = True
+False
+print(is_male)
+print(False)
