d108c74a40f5d572e56f99cf68b1e5dd24d15628
Roy wei <yuishang@gmail.com>
2024-03-20 14:25:58 +0800

    BMI calculator pj v20

--- practice/BMI Calculator pj.py
+++ practice/BMI Calculator pj.py
@@ -12,3 +12,16 @@ height = height / 100
 bmi = weight / height**2
 bmi = round(bmi, 1)
 print(f"你的BMI是:{bmi}")
+if bmi < 18.5:
+    print(f"你的BMI是:{bmi} (重過輕輕)")
+# elif bmi >= 18.5 and bmi < 24:
+elif bmi < 24:
+    print(f"你的BMI是:{bmi} (正常體位)")
+elif bmi < 27:
+    print(f"你的BMI是:{bmi} (體重過重)")
+elif bmi < 30:
+    print(f"你的BMI是:{bmi} (輕度肥胖)")
+elif bmi < 35:
+    print(f"你的BMI是:{bmi} (中度肥胖)")
+else:
+    print(f"你的BMI是:{bmi} (重度肥胖)")
