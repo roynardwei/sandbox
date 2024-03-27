9f149b5f3f33453a06b239cf436ab63941c7aa55
Roynardwei <yuishang@gmail.com>
2024-03-27 10:31:25 +0800

    函式練習-測驗at office

--- practice/day4.py
+++ practice/day4.py
@@ -8,3 +8,14 @@ def print_info(name, age, height):
 print_info("小黑", 26, 176)
 print_info(name="小綠", age=30, height=181)
 
+# 測驗一
+
+def find_max(num1, num2, num3):
+    if num1 > num2 and num1 > num3:
+        print(num1)
+    elif num2 > num1 and num2 > num3:
+        print(num2)
+    else:
+        print(num3)
+
+find_max(1, 2, 3)
