8d64d3689ac51332f21435191fd4e6a0618ad539
Roynardwei <yuishang@gmail.com>
2024-03-29 11:49:11 +0800

    測驗二Max-min

--- practice/day4.py
+++ practice/day4.py
@@ -24,16 +24,43 @@
 
 # 函式迴傳 return
 
+# def find_max(num1, num2, num3):
+#     if type(num1) != int or type(num2) != int or type(num3) != int:
+#         return "請輸入整數"
+#     print("你好")
+#     if num1 >=num2 and num1 >= num3:
+#         return num1
+#     elif num2 >=num1 and num2 >= num3:
+#         return num2
+#     else:
+#         return num3
+#
+# max = find_max("你好", 20, 30)
+# print(max)
+
+# 測驗2
+
 def find_max(num1, num2, num3):
-    if type(num1) != int or type(num2) != int or type(num3) != int:
-        return "請輸入整數"
-    print("你好")
-    if num1 >=num2 and num1 >= num3:
+
+    if num1 >= num2 and num1 >= num3:
         return num1
-    elif num2 >=num1 and num2 >= num3:
+    elif num2 >= num1 and num2 >= num3:
         return num2
     else:
         return num3
 
-max = find_max("你好", 20, 30)
-print(max)
+def find_min(num1, num2, num3):
+
+    if num1 <= num2 and num1 <= num3:
+        return num1
+    elif num2 <= num1 and num2 <= num3:
+        return num2
+    else:
+        return num3
+def max_min (num1, num2, num3):
+    if type(num1) != int or type(num2) != int or type(num3) != int:
+        return "請輸入整數"
+    else:
+        return find_max(num1, num2, num3)- find_min(num1, num2, num3)
+num = max_min(2, 5, 10)
+print(num)
