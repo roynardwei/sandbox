ab6b50e7ff8db1177919c7168aff5a2bdadca54a
Roynardwei <yuishang@gmail.com>
2024-04-22 15:00:04 +0800

    1-100質數練習

--- practice/whileloop2.py
+++ practice/whileloop2.py
@@ -30,6 +30,35 @@
 # continue跳出當前迴圈，跳到下一個迴圈
 
 
+#
+# number = 2
+# while number <= 100:
+#     i = 2
+#     is_prime = True
+#     while i < number:
+#         if number % i == 0:
+#             is_prime = False
+#             break
+#         i += 1
+#     if is_prime:
+#         print(number)
+#     number += 1
+
+# 輸出100以內的所有質數(質數是只能被1和它本身整除的數)
+# 循環2到100(遍歷)
+#   判斷當前的數字是不是質數(循環)
+#   印出
+#break跳出當前循環
+#continue跳出當前迴圈，跳到下一個迴圈
+# number = 6
+# i = 2
+# is_prime = True
+# while i < number:
+#     if number % i == 0:
+#         is_prime = False
+#         break
+#     i += 1
+# print(is_prime)
 
 number = 2
 while number <= 100:
@@ -43,3 +72,15 @@ while number <= 100:
     if is_prime:
         print(number)
     number += 1
+
+#
+# num = [];
+# i = 2
+# for i in range(2, 100):
+#     j = 2
+#     for j in range(2, i):
+#         if (i % j == 0):
+#             break
+#     else:
+#         num.append(i)
+# print(num)
