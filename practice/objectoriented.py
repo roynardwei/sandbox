668555c344dbe8b56dbc1876ead57a897547215a
Roynardwei <yuishang@gmail.com>
2024-06-04 14:12:23 +0800

    物件導向練習

---
+++ practice/objectoriented.py
@@ -0,0 +1,34 @@
+# 物件導向三大特性 封裝 繼承 多態
+# 封裝: 將物件屬性私有化, 不允許外部修改。 把共性(屬性, 函數)放一起成class
+# 繼承: 子類繼承父類的屬性, 函數
+# 多態: 同一個名字的函數, 擁有不同的效果
+
+class Animal:
+    def __init__(self, name):
+        self.name = name #加上兩個下底線防止繼承
+    def eat(self): #函數也可以加上兩個下底線防繼承
+        print("Animal的吃")
+    def sound(self):
+        pass
+
+
+class Dog(Animal):
+    def __init__(self):
+        super().__init__("小黃")
+    def sound(self):
+        print("汪汪")
+
+class Cat(Animal):
+    def __init__(self):
+        super().__init__("小黃")
+    def sound(self):
+        print("喵喵")
+def makesound(animal):
+    animal.sound()
+
+dog = Dog()
+cat = Cat()
+# dog.eat()
+# print(dog.name)
+makesound(dog) #多態
+makesound(cat)
