d5b237f68aa5da5c7732c19660d9e07ec82ae3ab
Roynardwei <yuishang@gmail.com>
2024-06-25 14:39:32 +0800

    裝飾器原理

---
+++ practice/decorator.py
@@ -0,0 +1,18 @@
+# 裝飾器本質是一個函數，把被裝飾的函數當成參數
+import time
+
+def timer(func):
+    def inner(*args, **kwargs):
+        for d in args:
+            print(d)
+        start = time.time()
+        func(*args, **kwargs)
+        print(time.time()-start)
+    return inner
+
+@timer
+def Test(a, b, c):
+    for i in range(100000):
+        print(123)
+
+Test(1, 2,3)
