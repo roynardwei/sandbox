e3534c8ee9ace6986c4c0bbf1a1e02922518057a
Roynardwei <yuishang@gmail.com>
2024-07-02 11:49:28 +0800

    use testlib

---
+++ practice/testlib.py
@@ -0,0 +1,6 @@
+# 自訂義一個module模組
+def add(a, b):
+    print(a+b)
+
+if __name__ == "__main__":
+    add(1, 3)
---
+++ practice/uselib.py
@@ -0,0 +1,9 @@
+#使用自訂義模組
+#運行裡面的所有代碼
+
+# import testlib
+from testlib import add
+
+# testlib.add(3,4)
+add(3, 4)
+
